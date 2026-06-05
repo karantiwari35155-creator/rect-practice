58da447ca0b053a754b7bb496f5f20aedb59aa2e
karantiwari35155-creator <karantiwari35155@gmail.com>
2026-06-05 23:34:14 +0530

    learn managing local storage using ract

--- local-storage/src/App.jsx
+++ local-storage/src/App.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 
 const App = () => {
+  // localStorage.clear()
+  sessionStorage.clear();
+  localStorage.setItem('class','A sec');
+  localStorage.getItem(name);
+  localStorage.removeItem('class')
+  
+  const user = {
+    username: "karan",
+    age: 18,
+    course: "BCA",
+  };
+
+  localStorage.setItem("user",JSON.stringify(user));
+  const parsed_data = JSON.parse(localStorage.getItem('user'));
+  console.log(parsed_data)
   return (
     <div>App</div>
   )
